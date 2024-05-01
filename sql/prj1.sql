b66ccf14945c0382489a7484013556ab79463052
jung tae wook <xodnr901901@naver.com>
2024-05-01 09:47:10 +0900

    2024/05/01-

---
+++ sql/prj1.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE prj1;
+
+USE prj1;
+
+DROP TABLE board;
+CREATE TABLE board
+(
+    id       INT PRIMARY KEY AUTO_INCREMENT,
+    title    VARCHAR(200)  NOT NULL,
+    content  VARCHAR(3000) NOT NULL,
+    writer   VARCHAR(100)  NOT NULL,
+    inserted DATETIME      NOT NULL DEFAULT NOW()
+);
---
+++ src/main/java/com/prj1/config/AppConfiguration.java
@@ -0,0 +1,12 @@
+package com.prj1.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+
+@Configuration
+@EnableMethodSecurity
+public class AppConfiguration {
+
+
+}
